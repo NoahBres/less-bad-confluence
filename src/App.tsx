ee25ad69c4cd4d07c4157105a1219e6930713eef
Noah Bresler <noahbresler2002@gmail.com>
2025-11-04 21:21:04 -0800

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,59 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import React from 'react';
+import { RouterProvider, createRouter, Outlet, createRootRoute, createRoute } from '@tanstack/react-router';
 
-import { routeTree } from './routeTree';
+import { Home } from './pages/Home';
+import { Page } from './pages/Page';
+import { Space } from './pages/Space';
+
+// Root route
+const rootRoute = createRootRoute({
+  component: () => React.createElement(Outlet),
+});
+
+// Home route
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: Home,
+});
+
+// Spaces route
+const spacesRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/spaces',
+});
+
+// Individual space route
+const spaceRoute = createRoute({
+  getParentRoute: () => spacesRoute,
+  path: '/$spaceKey',
+  component: function SpaceComponent() {
+    const { spaceKey } = spaceRoute.useParams();
+    return React.createElement(Space, { spaceKey });
+  },
+});
+
+// Pages route under a space
+const pagesRoute = createRoute({
+  getParentRoute: () => spaceRoute,
+  path: '/pages',
+});
+
+// Individual page route
+const pageRoute = createRoute({
+  getParentRoute: () => pagesRoute,
+  path: '/$pageId',
+  component: function PageComponent() {
+    const { spaceKey, pageId } = pageRoute.useParams();
+    return React.createElement(Page, { spaceKey, pageId });
+  },
+});
+
+// Create the route tree
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  spacesRoute.addChildren([spaceRoute.addChildren([pagesRoute.addChildren([pageRoute])])]),
+]);
 
 // Create a new router instance
 const router = createRouter({ routeTree });
