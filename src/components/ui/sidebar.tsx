fe6da6e80f90a650357933c0e2b684ac453b115c
Noah Bresler <noahbresler2002@gmail.com>
2025-11-07 22:48:10 -0800

    fix: correct CSS class syntax for disabling animation during sidebar dragging

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -275,7 +275,7 @@ const Sidebar = React.forwardRef<
               ? 'group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]'
               : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon)',
             //* set duration to 0 for all elements when dragging
-            'group-data-[dragging=true]_*:!duration-0 group-data-[dragging=true]:duration-0!'
+            'group-data-[dragging=true]_*:duration-0! group-data-[dragging=true]:duration-0!'
           )}
         />
         <div
@@ -289,7 +289,7 @@ const Sidebar = React.forwardRef<
               ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]'
               : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l',
             //* set duration to 0 for all elements when dragging
-            'group-data-[dragging=true]_*:!duration-0 group-data-[dragging=true]:duration-0!',
+            'group-data-[dragging=true]_*:duration-0! group-data-[dragging=true]:duration-0!',
             className
           )}
           {...props}
